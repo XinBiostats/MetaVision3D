f3808c453a6cfdfd123236ce53e6217a770b3cb5
Xin Ma <136360597+XinBiostats@users.noreply.github.com>
2024-05-06 11:07:13 -0400

    Delete modules directory

--- modules/utils.py
+++
@@ -1,8 +0,0 @@
-# utils
-import pandas as pd
-import numpy as np
-
-def get_ref_compound(df,first_feature='LPA_16_0_'):
-    first_feature_index = np.where(df.columns==first_feature)[0][0]
-    prevalence = df.iloc[:,first_feature_index:].astype(bool).mean()
-    return prevalence.idxmax()
